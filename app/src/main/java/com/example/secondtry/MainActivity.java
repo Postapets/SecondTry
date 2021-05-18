29e6bf0024bf0ee6b88c34070a7c4ff70af4b076
Postapets <71009041+Postapets@users.noreply.github.com>
2021-05-18 20:09:30 +0300

    Приделал сплеш скрин Кнопки меняют цвет,  но надо допилить

--- app/src/main/java/com/example/secondtry/MainActivity.java
+++ app/src/main/java/com/example/secondtry/MainActivity.java
@@ -34,7 +34,6 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this.getApplicationContext(), AddActivity.class);
         startActivity(intent);
     }
-
     public void delete(){
 
     }
--- app/src/main/java/com/example/secondtry/SplashScreen.java
+++ app/src/main/java/com/example/secondtry/SplashScreen.java
@@ -16,4 +16,5 @@ public class SplashScreen extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+
 }
